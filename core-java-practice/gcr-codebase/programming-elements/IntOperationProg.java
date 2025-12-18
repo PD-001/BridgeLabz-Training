e467d461b9648137338ba08f6be13f4c15f869ad
Prakash Dixit <prakashdixit879@gmail.com>
2025-12-18 14:42:38 +0530

    [Prakash Dixit] Add. Solutions to Level 2 programming elements problems on 18 Dec.

---
+++ core-java-practice/gcr-codebase/programming-elements/IntOperationProg.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;;
+
+public class IntOperationProg {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int a= sc.nextInt();
+        int b= sc.nextInt();
+        int c= sc.nextInt();
+
+        // performing integer operations
+        int op1= a+b*c;
+        int op2= a*b+c;
+        int op3= c+a/b;
+        int op4= a%b+c;
+
+        System.out.println("The results of Int Operations are "+ op1 +", "+ op2 +", "+ op3 +", and "+ op4 +".");
+        sc.close();
+    }
+}
